8fb66fa591e7b3a98e444289f32a366c8ee770a2
Gonzalo Albornoz <galbornoz@sgfintech.com.ar>
2024-12-28 20:30:29 -0300

    scores al final y en game over reinicia al menu

--- RPG-Game/Game.h
+++ RPG-Game/Game.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 #include "Player.h"
 #include "Stage.h"
 #include <vector>
@@ -51,14 +52,16 @@ private:
 	Text* mainMenuTitle = new Text(80, sf::Color::White);
 	Text* mainMenuNav = new Text(30, sf::Color::White);
 
-	Text* controlsMenuText = new Text(40, sf::Color::White);
+	Text* controlsMenuText = new Text(30, sf::Color::White, "consolas");
 
 	Text* menuNavText = new Text(30, sf::Color::White);
+	Text* menuNavText2 = new Text(30, sf::Color::White);
+	Text* menuNavText3 = new Text(30, sf::Color::White);
 
 	Text* scoresText = new Text(22, sf::Color::White, "consolas");
 
-	Text* menuPlayerName = new Text(24, sf::Color::White);
-	Text* playerNameText = new Text(22, sf::Color::White, "consolas");
+	Text* menuPlayerName = new Text(32, sf::Color::White);
+	Text* playerNameText = new Text(24, sf::Color::White, "consolas");
 
 	Character* finalBoss = nullptr;
 
@@ -67,6 +70,8 @@ private:
 
 	std::string controlsStr = "Controls: \n \nLeft Arrow (Left)\nRight Arrow (Right)\nUp Arrow (Jump)\nSPACE (Attack)";
 	std::string navMenu = "Esc to Back";
+	std::string navMenu2 = "Enter to Start\nEsc to Back";
+	std::string navMenu3 = "Enter to Main Menu";
 
 	std::string scoreString = "";
 
@@ -78,6 +83,48 @@ private:
 
 	std::string defaultPlayerName = "SIEGWARD";
 	std::string playerName = defaultPlayerName;
+
+	void UpdateScore() {
+		std::vector<ScoreRecord> scores;
+
+		std::ifstream filein("data/score.dat", std::ios::binary);
+
+		ScoreRecord scoreTemp;
+		while (filein.read(reinterpret_cast<char*>(&scoreTemp), sizeof(ScoreRecord))) {
+			for (auto& c : scoreTemp.name) c = toupper(c);
+			scores.push_back(scoreTemp);
+		}
+		filein.close();
+		
+		std::string _playerName = playerName;
+		auto mismoPlayerScoreIt = std::find_if(scores.begin(), scores.end(), [_playerName](ScoreRecord sr) { return sr.name == _playerName;});
+
+		if (mismoPlayerScoreIt != scores.end()) {
+			if (mismoPlayerScoreIt->points >= points) {
+				return;
+			}
+			else {
+				mismoPlayerScoreIt->points = points;
+			}
+		}
+		else {
+			ScoreRecord sr;
+			strcpy_s(sr.name, playerName.c_str());
+			sr.points = points;
+			scores.push_back(sr);
+		}
+		
+		std::sort(scores.begin(), scores.end(), [](ScoreRecord sr1, ScoreRecord sr2) { return sr1.points > sr2.points; });
+		scores.pop_back();
+
+		std::ofstream fileout("data/score.dat", std::ios::binary);
+
+		for (auto& score : scores) {
+			fileout.write(reinterpret_cast<char*>(&score), sizeof(score));
+		}
+
+		fileout.close();
+	}
 
 	void LoadScores() {
 		std::ifstream file("data/score.dat", std::ios::binary);
@@ -102,9 +149,17 @@ private:
 	}
 	
 	void LoadMenu() {
+
+		if (stage != nullptr) {
+			delete stage;
+			stage = nullptr;
+		}
+
 		stage = new Stage(-1);
 		cursorMainMenu = new sf::CircleShape(7);
 		cursorMainMenu->setFillColor(sf::Color::White);
+
+		scoreString = "";
 
 		cursorInitialPos = sf::Vector2f(550, 380);
 		cursorMainMenu->setPosition(cursorInitialPos);
@@ -126,12 +181,22 @@ private:
 		menuNavText->CenterOrigin();
 		menuNavText->CenterOrigin();
 
+		menuNavText2->SetString(navMenu2);
+		menuNavText2->SetPos((width / 2), (height - 70));
+		menuNavText2->CenterOrigin();
+		menuNavText2->CenterOrigin();
+
+		menuNavText3->SetString(navMenu3);
+		menuNavText3->SetPos((width / 2), (height - 40));
+		menuNavText3->CenterOrigin();
+		menuNavText3->CenterOrigin();
+
 		menuPlayerName->SetString("Player name: ");
-		menuPlayerName->SetPos(500, (height / 2));
+		menuPlayerName->SetPos(550, (height / 2));
 		menuPlayerName->CenterOrigin();
 
 		playerNameText->SetString(playerName);
-		playerNameText->SetPos(700, (height / 2));
+		playerNameText->SetPos(700, (height / 2) - 5);
 		playerNameText->CenterOrigin();
 
 	}
@@ -182,6 +247,11 @@ private:
 		window.setFramerateLimit(60);
 		window.setKeyRepeatEnabled(false);
 
+		if (stage != nullptr) {
+			delete stage;
+			stage = nullptr;
+		}
+
 		stage = new Stage(current_stage);
 		stage->LoadEnemies(enemies, siegward);
 		siegward->SetPos(player_initial_pos);
@@ -203,6 +273,25 @@ private:
 					else if (event.key.code == sf::Keyboard::Up) {
 						mainMenuCursorSelected--;
 						if (mainMenuCursorSelected < 0) mainMenuCursorSelected = (mainMenuCursorSelectedMax - 1);
+					}
+					else if (event.key.code == (sf::Keyboard::Enter)) {
+
+						switch (mainMenuCursorSelected)
+						{
+						case 0:
+							//gameState = STATE__GAME_RUNNING;
+							//InitializeGame();
+							gameState = STATE__MENU_ADD_NAME;
+							break;
+						case 1:
+							gameState = STATE__MENU_CONTROLS;
+							break;
+						case 2:
+							gameState = STATE__MENU_SCORES;
+							break;
+						default:
+							break;
+						}
 					}
 
 				}
@@ -229,6 +318,12 @@ private:
 						gameState = STATE__MENU;
 					}
 				}
+				else if (gameState == STATE__GAME_RUNNING) {
+					if (isGameOver && event.key.code == (sf::Keyboard::Enter)) {
+						LoadMenu();
+						gameState = STATE__MENU;
+					}
+				}
 
 			}
 
@@ -236,7 +331,7 @@ private:
 	}
 
 	void UpdateGame() {
-		if (!(siegward->isAlive() && (finalBoss == nullptr || finalBoss->isAlive()))) { isGameOver = true; return; };
+		if (isGameOver) { return; };
 
 		sf::Vector2f playerPos = siegward->GetPos();
 
@@ -308,9 +403,6 @@ private:
 
 		}
 
-		//for (auto entity : entities) {
-		//	entity->Update();
-		//}
 		siegward->Update();
 		for (auto enemy : enemies) {
 			enemy->Update();
@@ -324,16 +416,16 @@ private:
 
 		stageText->SetString("Stage : " + std::to_string(current_stage));
 		pointsText->SetString("Points : " + std::to_string(points));
+
+		if (!siegward->isAlive() || (finalBoss != nullptr && !finalBoss->isAlive())) {
+			UpdateScore();
+			isGameOver = true;
+		};
 	}
 
 	void Update() {
 		if (gameState == STATE__GAME_RUNNING) {
-			if (isGameOver) {
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
-					InitializeGame();
-				}
-			}
-			else {
+			if (!isGameOver) {
 				UpdateGame();
 			}
 		}
@@ -342,25 +434,7 @@ private:
 			newPos.y = newPos.y + mainMenuCursorSelected * 38;
 			cursorMainMenu->setPosition(newPos);
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
-				switch (mainMenuCursorSelected)
-				{
-				case 0:
-					//gameState = STATE__GAME_RUNNING;
-					//InitializeGame();
-					gameState = STATE__MENU_ADD_NAME;
-					break;
-				case 1:
-					gameState = STATE__MENU_CONTROLS;
-					break;
-				case 2:
-					gameState = STATE__MENU_SCORES;
-					break;
-				default:
-					break;
-				}
 
-			}
 		}
 		else if (gameState == STATE__MENU_SCORES) {
 			if (scoreString.empty()) {
@@ -393,43 +467,44 @@ private:
 
 		stageText->Draw(window);
 		pointsText->Draw(window);
+
 		//game over region
+		if (isGameOver) {
+			if (finalBoss != nullptr && !finalBoss->isAlive()) {
+				Text winTxt(40, sf::Color::White);
+				winTxt.SetString("You've killed the menace in the catacombs. Congratulations!");
+				winTxt.CenterOrigin();
+				winTxt.SetPos((width / 2), (height / 2));
+				//auto bounds = text.getGlobalBounds();
+				//text.setPosition;
+
+				winTxt.Draw(window);
+			}
+			else {
+				//mover todo esto a una clase texto
+
+				Text gameoverTxt(40, sf::Color::White);
+				gameoverTxt.SetString("Game Over");
+				gameoverTxt.CenterOrigin();
+				gameoverTxt.SetPos((width / 2), (height / 2));
+
+				//auto bounds = text.getGlobalBounds();
+				//text.setPosition;
+
+				gameoverTxt.Draw(window);
+			}
+		}
 		if (!siegward->isAlive()) {
-			//mover todo esto a una clase texto
 
-			Text gameoverTxt(40, sf::Color::White);
-			gameoverTxt.SetString("Game Over");
-			gameoverTxt.CenterOrigin();
-			gameoverTxt.SetPos((width / 2), (height / 2));
-
-			//auto bounds = text.getGlobalBounds();
-			//text.setPosition;
-
-			gameoverTxt.Draw(window);
 		}
-
-
-		//end game over region
-
-		//win region
-		if (finalBoss != nullptr && !finalBoss->isAlive()) {
-			Text winTxt(40, sf::Color::White);
-			winTxt.SetString("You've killed the menace in the catacombs. Congratulations!");
-			winTxt.CenterOrigin();
-			winTxt.SetPos((width / 2), (height / 2));
-
-
-			//auto bounds = text.getGlobalBounds();
-			//text.setPosition;
-
-			winTxt.Draw(window);
-		}
-		//end win region
 	}
 
 	void Draw() {
 		if (gameState == STATE__GAME_RUNNING) {
 			DrawGame();
+			if (isGameOver) {
+				menuNavText3->Draw(window);
+			}
 		}
 		else if (gameState == STATE__MENU) {
 			stage->Draw(window);
@@ -451,7 +526,7 @@ private:
 			stage->Draw(window);
 			playerNameText->Draw(window);
 			menuPlayerName->Draw(window);
-			menuNavText->Draw(window);
+			menuNavText2->Draw(window);
 		}
 	}
 
@@ -476,8 +551,6 @@ public:
 
 			Render();
 		}
-
-		
 	}
 
 };
